319ab64d90687111ec0139e1f8343b786edee55a
amirn003 <amir.naar@gmail.com>
2023-11-23 14:56:47 +0100

    add countdown counter

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({now, then});
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondLeft = Math.round((then - Date.now()) / 1000);
+    if (secondLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }

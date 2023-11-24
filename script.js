e9c286a215a950654a18f981f0bfc0dfa78fab92
amirn003 <amir.naar@gmail.com>
2023-11-24 14:34:28 +0100

    add end time counter

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondLeft = Math.round((then - Date.now()) / 1000);
@@ -19,7 +22,15 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const reminderSeconds = seconds % 60;
-  const display = `${minutes}:${reminderSeconds}`;
+  const display = `${minutes}:${reminderSeconds < 10 ? '0' : ''}${reminderSeconds}`;
   timerDisplay.textContent = display;
+  document.title = display;
   console.log({minutes, reminderSeconds});
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minute = end.getMinutes();
+  endTime.textContent = `Be Back at ${hour}:${minute}`;
 }
